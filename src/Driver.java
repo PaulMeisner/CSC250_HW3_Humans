19e5a8f824c26a044d04e340f88cb672ddbebac0
PaulMeisner <paul.meisner@cuw.edu>
2021-02-17 10:20:06 -0600

    initial push

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,60 @@
+import java.net.URL;
+import java.util.Scanner;
+
 public class Driver 
 {
 	public static void main(String[] args)
 	{
-		attributes a1 = new attributes("Paul", "Meisner", 21, "male" );
+		attributes a1 = new attributes("Peter Parker", "SPider Man", "unknown", "good", "male");
 		
 		
 		a1.display();
-
+		
+		
+		//Scanner input = new Scanner(System.in);
+		//System.out.print("Enter your name:");
+		//String name = input.nextLine();
+		//System.out.println(name);
+		
+		//Scanner input1 = new Scanner(System.in);
+		//System.out.print("Enter your age:");
+		//String age = input.nextLine();
+		//System.out.println(Integer.parseInt(age) + 2);
+		//int age = input.nextInt();
+		//System.out.println(age + 2);
+		
+		
+		String cardJson = Driver.getJSON("https://datahub.io/five-thirty-eight/comic-characters/r/1.html");
+		System.out.println(cardJson);
+	}
+		
+		static String getJSON(String urlString)
+		{
+			String line = "";
+			try
+			{
+				URL url = new URL(urlString);
+				Scanner input = new Scanner(url.openStream());
+				//open url stream, 
+				//keep reading froms canner as long as their is something to read
+				
+				while (input.hasNext())
+				{
+					line += input.nextLine();
+				}
+				
+				input.close();
+				//reader close
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+				line = "Cant connect";
+			}
+			
+			
+		
+		return line;
+		
 	}
 }
--- src/attributes.java
+++ src/attributes.java
@@ -1,20 +1,23 @@
 public class attributes
 {
-	private int age;
-	private String firstName;
-	private String lastName;
+	private String name;
+	private String heroName;
+	private String identity;
+	private String align;
 	private String gender;
 	
-	public attributes(String firstName, String lastName, int age, String gender)
+	public attributes(String name, String heroName, String identity, String align, String gender)
 	{
-		this.age = age;
-		this.firstName = firstName;
-		this.lastName = lastName;
+	
+		this.name = name;
+		this.heroName = heroName;
+		this.identity = identity;
+		this.align = align;
 		this.gender = gender;
 	}
 	
 	void display()
 	{
-		System.out.println("First name: " + this.firstName + "\nLast Name: " + this.lastName + "\nAge: " + this.age + "\nGender: " + this.gender);
+		System.out.println("Name: " + this.name + "\nHero Name: " + this.heroName + "\nIdentity: " + this.identity + "\nAlign: " + this.align + "\nGender: " + this.gender);
 	}
 }
